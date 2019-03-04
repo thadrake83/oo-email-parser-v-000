115653eb98a6d0d221d1b2b267dd476820bef287
Drake Rentrop <drakerentrop@gmail.com>
2019-03-04 19:03:47 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,15 +4,15 @@
 # or whitespace (' ').
 class EmailParser
 	attr_accessor :emails
-	
+
 	def initialize(emails)
 		@emails= emails
 	end
-	
+
 	def parse
 		@emails = @emails.gsub(/[\s,]/ ," ").split
 		# @email is an array now
 		@emails.uniq
 	end
-	
+
 end
